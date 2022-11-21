58bb3f1c529268e8c2ec823e8690a306ca3c778d
azizularif109 <mdazizularif109@gmail.com>
2022-11-21 21:39:36 +0600

    vite and laravel setup completed

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,14 @@
-import './bootstrap';
+import "./bootstrap";
+import "../css/app.css";
+import "../sass/app.scss";
+
+import { createApp } from "vue/dist/vue.esm-bundler";
+import { createRouter, createWebHistory } from "vue-router";
+// import router from "./routes";
+
+const app = createApp({
+    components: {},
+});
+
+app.mount("#app");
+// app.use(router);
--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,21 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                "resources/backend/vendor/jquery/jquery.min.js",
+                "resources/backend/vendor/bootstrap/js/bootstrap.bundle.min.js",
+                "resources/backend/vendor/jquery-easing/jquery.easing.min.js",
+                "resources/backend/js/ruang-admin.min.js",
+                "resources/backend/vendor/chart.js/Chart.min.js",
+                "resources/backend/js/demo/chart-area-demo.js",
+                "resources/backend/vendor/fontawesome-free/css/all.min.css",
+                "resources/backend/vendor/bootstrap/css/bootstrap.min.css",
+                "resources/backend/css/ruang-admin.min.css",
+                "resources/css/app.css",
+            ],
             refresh: true,
         }),
     ],
